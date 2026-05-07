a5a13b710b189c2547365c94d6ebc10378aa9f49
VisaliniKJ2007 <visalini.kj2024cse@sece.ac.in>
2026-05-07 10:21:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+    let filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (err, content) => {
+
+        if (err) {
+            res.writeHead(500);
+            res.end("Server Error");
+        } else {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(content, "utf-8");
+        }
+
+    });
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
